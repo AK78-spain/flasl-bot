bba422d5101a498fef94ee8ee27a753a02bd5f6c
AK78-spain <alimohassel096@gmail.com>
2025-08-06 08:34:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ def webhook():
 
 # ------------------ اجرای محلی ------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
